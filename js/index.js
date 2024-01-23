36bad5601c4b1ecb800cd40d2151f1eb5005749e
webgurusam <shyamghosh666@gmail.com>
2024-01-23 16:32:47 +0600

    added calculation functionalities

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
+let total = 0;
 function handleClickBtn(data){
     const selectedItemContainer = document.getElementById('selected-items');
     const itemName = data.parentNode.childNodes[1].innerText;
     const li = document.createElement('li');
-    li.innerText = itemName;
-    selectedItemContainer.appendChild(li);
-    const price = data.parentNode.childNodes[5].innerText.split(" ")[2];
-    console.log(price)
+    const count = selectedItemContainer.childElementCount;
+    if((count+1) <= 5){
+        li.innerHTML = `${count+1}. ${itemName}`;
+        li.style.listStyleType = 'none';
+
+        selectedItemContainer.appendChild(li);
+        const price = data.parentNode.childNodes[5].innerText.split(" ")[2];
+        total += parseFloat(price);
+        document.getElementById('total').innerText = total;
+    }
+    else{
+        alert('you can not select more than 5 items');
+    }
+
+}
+function submitCouponCode(){
+    const discount = document.getElementById('discount');
+    const payableAmount = document.getElementById('payable-amount');
+
+    if(document.getElementById('discount-field').value === 'HERO'){
+        discount.innerText = (total * 20 / 100)
+        payableAmount.innerText = total - (total * 20 / 100);
+    }
+    else{
+        payableAmount.innerText = total;
+    }
 }
