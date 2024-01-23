a558a10a05a043cf7c94831e28e1d513d7620744
webgurusam <shyamghosh666@gmail.com>
2024-01-23 11:34:12 +0600

    adding JS for calculation function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('index.js file is connected');
+function handleClickBtn(data){
+    const selectedItemContainer = document.getElementById('selected-items');
+    const itemName = data.parentNode.childNodes[1].innerText;
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    selectedItemContainer.appendChild(li);
+    const price = data.parentNode.childNodes[5].innerText.split(" ")[2];
+    console.log(price)
+}
